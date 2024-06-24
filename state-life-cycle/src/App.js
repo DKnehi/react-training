75630fbf3727b5b4d9c4819b36006d2f2e3741b1
Truong Quoc Dat <truongquocdatlqd@gmail.com>
2024-06-24 17:17:19 +0700

    Create example state-life-cycle

---
+++ state-life-cycle/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import ComponentMemory from './components/ComponentMemory';
+import MultipleState from './components/MultipleState';
+function App() {
+  return (
+    <div>
+      <ComponentMemory></ComponentMemory>
+      <MultipleState></MultipleState>
+      <MultipleState></MultipleState>
+    </div>
+  );
+}
+
+export default App;
