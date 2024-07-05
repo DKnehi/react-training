ed79c1291ab22add73c49842425e036fa0e0575b
Truong Quoc Dat <truongquocdatlqd@gmail.com>
2024-07-05 14:45:56 +0700

    setup environment and create themes

---
+++ react-basic-practice/src/themes/variables.tsx
@@ -0,0 +1,28 @@
+export const fontSizes = {
+  xxs: "11px",
+  xs: "12px",
+  s: "14px",
+  m: "22px",
+};
+
+export const fontWeights = {
+  light: "400",
+  normal: "500",
+  bold: "600",
+};
+
+export const colors = {
+  riverbed: "#464F60",
+  mirage: "#171C26",
+  palesky: "#687182",
+  shuttlegray: "#5A6376",
+  white: "#FFFFFF",
+  brickred: "#D12953",
+  royalblue: "#4F5AED",
+  jewel: "#14804A",
+};
+
+export const borderRadius = {
+  xxs: "5px",
+  xs: "6px",
+};
