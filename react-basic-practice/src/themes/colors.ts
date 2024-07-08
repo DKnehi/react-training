5d3df6fc4145058c4751c1c9e785fb9b4b82ea29
Truong Quoc Dat <truongquocdatlqd@gmail.com>
2024-07-08 14:46:25 +0700

    remove file not use setupTest.ts, App.test. shorthand color

--- react-basic-practice/src/themes/colors.ts
+++ react-basic-practice/src/themes/colors.ts
@@ -3,7 +3,7 @@ export const colors = {
   mirage: "#171C26",
   paleSky: "#687182",
   shuttleGray: "#5A6376",
-  white: "#FFFFFF",
+  white: "#FFF",
   brickRed: "#D12953",
   royalBlue: "#4F5AED",
   jewel: "#14804A",
--- react-basic-practice/src/themes/index.ts
+++ react-basic-practice/src/themes/index.ts
@@ -1,7 +1,7 @@
 import { extendTheme } from "@chakra-ui/react";
 import { fonts, fontSizes, fontWeights } from "./typography";
 import { colors } from "./colors";
-import { borderRadius } from "./metric";
+import { borderRadius } from "./metrics";
 
 const theme = extendTheme({
   fonts,
