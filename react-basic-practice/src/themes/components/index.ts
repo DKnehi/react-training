4d89ed407af3676c55e2a220284fcc84e01ff68b
Truong Quoc Dat <truongquocdatlqd@gmail.com>
2024-07-16 17:55:50 +0700

    Update index theme component

--- react-basic-practice/src/themes/components/index.ts
+++ react-basic-practice/src/themes/components/index.ts
@@ -1,9 +1,6 @@
 import { Button } from "./button";
 import { Input } from "./input";
 
-export { Button } from "./button";
-export { Input } from "./input";
-
 export const components = {
   Button,
   Input,
