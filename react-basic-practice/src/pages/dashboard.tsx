4e6b0142580b99a1d987417888f78442a9dc23ce
Truong Quoc Dat <truongquocdatlqd@gmail.com>
2024-07-26 14:29:29 +0700

    Render data customer from mock API

--- react-basic-practice/src/pages/dashboard.tsx
+++ react-basic-practice/src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Text } from "@chakra-ui/react";
 import { ICustomer, ActionType } from "@types";
 import {
@@ -11,12 +11,22 @@ import {
   CustomerView,
 } from "@components";
 import { MODAL_TITLES, MODAL_DESCRIPTION } from "@constants";
+import { fetchUsers } from "@services";
 
 const Dashboard: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [modalTitle, setModalTitle] = useState("Add Customer");
   const [customerData, setCustomerData] = useState<ICustomer>();
   const [isConfirmDelete, setIsConfirmDelete] = useState(false);
+  const [data, setData] = useState<ICustomer[]>([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const users = await fetchUsers();
+      setData(users);
+    };
+    getUsers();
+  }, []);
 
   const handleOpenModal = (title: string, data?: ICustomer) => {
     setModalTitle(title);
@@ -66,56 +76,12 @@ const Dashboard: React.FC = () => {
         );
 
       case MODAL_TITLES.VIEW_CUSTOMER:
-        return customerData ? <CustomerView data={customerData} /> : null;
+        return <CustomerView data={customerData!} />;
 
       default:
         return <CustomerForm data={customerData} />;
     }
   };
-
-  // Mock data
-  const data: ICustomer[] = [
-    {
-      name: "Ann Culhane",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus nobis non obcaecati reiciendis pariatur. Perferendis pariatur labore, atque mollitia veniam voluptatum cum deserunt facilis at enim, fuga, provident inventore rem.",
-      status: "Open",
-      rate: "$70.00",
-      balance: "-$270.00",
-      deposit: "$500.00",
-      id: 1,
-    },
-    {
-      name: "user name",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus nobis non obcaecati reiciendis pariatur. Perferendis pariatur labore, atque mollitia veniam voluptatum cum deserunt facilis at enim, fuga, provident inventore rem.",
-      status: "Paid",
-      rate: "$70.00",
-      balance: "$270.00",
-      deposit: "$500.00",
-      id: 2,
-    },
-    {
-      name: "user name",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus nobis non obcaecati reiciendis pariatur. Perferendis pariatur labore, atque mollitia veniam voluptatum cum deserunt facilis at enim, fuga, provident inventore rem.",
-      status: "Due",
-      rate: "$70.00",
-      balance: "-$270.00",
-      deposit: "$500.00",
-      id: 3,
-    },
-    {
-      name: "user name",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus nobis non obcaecati reiciendis pariatur. Perferendis pariatur labore, atque mollitia veniam voluptatum cum deserunt facilis at enim, fuga, provident inventore rem.",
-      status: "Inactive",
-      rate: "$70.00",
-      balance: "$270.00",
-      deposit: "$500.00",
-      id: 4,
-    },
-  ];
 
   return (
     <Box>
@@ -133,7 +99,6 @@ const Dashboard: React.FC = () => {
           onClick={handleAddCustomer}
         />
       </Box>
-      {/* Table demo */}
       <Table columns={TableColumn} data={data} action={handleAction} />
       <Modal
         isOpen={isOpen}
