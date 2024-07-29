98e38e6bd4d978de51622af464a31a5af10abe90
Truong Quoc Dat <truongquocdatlqd@gmail.com>
2024-07-29 08:25:28 +0700

    remove randudant code

--- react-basic-practice/src/services/user.ts
+++ react-basic-practice/src/services/user.ts
@@ -37,27 +37,3 @@ export const deleteUser = async (id: number) => {
     }
   }
 };
-
-export const createUser = async (customer: ICustomer) => {
-  try {
-    const response = await fetch(`${API.BASE_URL}/${API.ENDPOINT_USERS}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(customer),
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to create the user");
-    }
-
-    return await response.json();
-  } catch (error) {
-    if (error instanceof Error) {
-      throw new Error(error.message);
-    } else {
-      throw new Error("An unknown error occurred");
-    }
-  }
-};
