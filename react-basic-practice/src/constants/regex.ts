1fc6109e4e0cc03b88a2cc3a54ca005eaf21f894
Truong Quoc Dat <truongquocdatlqd@gmail.com>
2024-08-06 09:01:35 +0700

    update regex

--- react-basic-practice/src/constants/regex.ts
+++ react-basic-practice/src/constants/regex.ts
@@ -1,3 +1,3 @@
 export const TEXT = /^[a-zA-Z\s]+$/;
-export const POSITIVE_DECIMAL = /^\d{0,9}(\.\d{0,2})?$/;
-export const NEGATIVE_ALLOWED = /^-?\d{0,9}(\.\d{0,2})?$/;
+export const POSITIVE_DECIMAL = /^[a-zA-Z0-9\s]{0,9}(\.[a-zA-Z0-9\s]{0,2})?$/;
+export const NEGATIVE_ALLOWED = /^-?[a-zA-Z0-9\s]{0,9}(\.[a-zA-Z0-9\s]{0,2})?$/;
